6aa7e72b6f5dcfb777ba274db6ba38cb599753af
wveik <kataev_06@list.ru>
2016-04-19 15:32:53 +0400

    Second commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
     //Device Event Listener
     document.addEventListener("deviceready", onDeviceReady, false);
+    
+    $('#main_btn').bind('click', main_btn_click);
 });
+
+function capitalizeFirstLetter(string) {
+    
+    string = string.toLowerCase();
+    
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 
 // Check Device is Ready
@@ -35,7 +44,28 @@ function getDate(){
 
 // Get Current User Location
 function getLocation(){    
-    navigator.geolocation.getCurrentPosition(showPosition);
+    //navigator.geolocation.getCurrentPosition(showPosition, errorFunction);
+    //navigator.geolocation.watchPosition(showPosition, errorFunction);
+    if (navigator !== null) {
+        if (navigator.geolocation) {
+            var options = {
+                    enableHighAccuracy: true,
+                     timeout: 15000,
+                     maximumAge: 0
+             };
+             try {
+                     navigator.geolocation.getCurrentPosition(function (position) {
+                            showPosition(position);
+                      }, function () {
+                        alert('Do not find position');
+                      }, options);
+              } catch (e) {
+                  // ooops
+
+            }
+        }
+    }
+
 }
 
 function showPosition(position){    
@@ -52,7 +82,8 @@ function showPosition(position){
       url: url
     })
     .done(function( location ) {
-        getWeather(location);
+        var city = location.results[0].address_components[2].long_name;
+        getWeather(city);
     });    
     
 }
@@ -63,25 +94,46 @@ function getLocationPicture(position){
 
     var path = "http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=13&size=300x300&sensor=false";
 
+    getImageFromGoogleMaps(path);
+}
+
+function getImageFromGoogleMaps(path){
     var img = new Image();
     img.src = path;
     img.width = '100';
 
-    var output = document.getElementById("more_weather_display");
-    output.appendChild(img);
+    $('#more_weather_display').html(img); 
 }
 
-function getWeather(location){
+function getWeather(city){
+    
+    $('#myLocation').html(city);
     
     $.simpleWeather({
-        location: location.results[0].formatted_address,
+        location: city,
         woeid: '',
         unit: 'c',
         success: function(weather) {
-          console.log(weather);
+          //console.log(weather);
+            $('#weather').html('<h1>' + weather.temp + ' C&deg;</h1>');
         },
         error: function(error) {
-          $("#weather").html('<p>'+error+'</p>');
+          //$("#weather").html('<p>'+error+'</p>');
+            console.log(error);
         }
       });
+}
+
+function main_btn_click(){
+    var city = capitalizeFirstLetter($('#city').val());
+    getLocationPictureByCityName(city);
+    getWeather(city);
+}
+
+
+
+function getLocationPictureByCityName(city){
+    var path = "http://maps.googleapis.com/maps/api/staticmap?center=" + city + "&zoom=13&size=300x300&sensor=false";
+
+    getImageFromGoogleMaps(path);
 }
